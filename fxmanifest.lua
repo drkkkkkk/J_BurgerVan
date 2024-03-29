39e5a939d979546e5123170785877063933493d2
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-03-29 01:23:51 +0000

    Burber Van
    Burberrrrrrrrrrrrr!

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+
+files {
+	'data/**/carcols.meta',
+	'data/**/carvariations.meta',
+	'data/**/contentunlocks.meta',
+	'data/**/handling.meta',
+	'data/**/vehiclelayouts.meta',
+	'data/**/vehicles.meta',
+	'data/**/caraddonContentUnlocks.meta'
+}
+
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/contentunlocks.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/caraddoncontentunlocks.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
