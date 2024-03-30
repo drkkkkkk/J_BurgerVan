3c9b66e5df7aa9977a73db5c114542c54b9d8f7a
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-03-30 02:20:13 +0000

    Update fxmanifest.lua
    FINALLY FIXED TEXTURE BUG!

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,10 @@ game 'gta5'
 files {
 	'data/**/handling.meta',
 	'data/**/vehicles.meta',
+	'data/**/carvariations.meta',
 }
 
 
 data_file 'HANDLING_FILE' 'data/**/handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
